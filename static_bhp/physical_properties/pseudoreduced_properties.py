813ecfc1ac62b3ff842b4aa468e84b73101fc41f
betiniakarandut <betiniakarandut@gmail.com>
2023-07-13 03:51:31 +0100

    Code refactoring

--- static_bhp/physical_properties/pseudoreduced_properties.py
+++ static_bhp/physical_properties/pseudoreduced_properties.py
@@ -1,4 +1,5 @@
-from PseudocriticalProperties import *
+from conversions import temp_avg_in_rankine
+from pseudocritical_properties import natural_gas_systems, natural_gas_systems2
 
 # COMPUTING PSEUDOREDUCED PROPERTIES
 # gas_type = input('type in a gas system:Natural gas system or gas condensate system? ')
@@ -6,15 +7,11 @@ from PseudocriticalProperties import *
 def pseudo_reduced_temp():
     """Function to compute reduced temperature
 
-
-    Return:
+    Return
         floats: reduced temperature rounded to one
         decimal place
-
     """
-
     tpr_pseudo_reduced_temp = float(temp_avg_in_rankine()) / natural_gas_systems()
-
     return round(tpr_pseudo_reduced_temp, 1)
 
 
@@ -25,15 +22,11 @@ def pseudo_reduced_wellhead_pressure():
     """Function to compute reduced pressure
     at wellhead pressure
 
-
     Return:
         floats: reduced pressure at wellhead rounded
         to three decimal places
-
     """
-
     ppr1_wellhead = static_wellhead_pressure / natural_gas_systems2()
-
     return round(ppr1_wellhead, 3)
 
 
@@ -42,7 +35,7 @@ input('using the values of Tpr and Ppr displayed below. Goto sukkarcornel integr
 
 print('')
 print('<-------------------------------------->')
-print(f"REDUCED TEMP= {pseudo_reduced_temp()}")
-print(f"REDUCED PRESSURE = {pseudo_reduced_wellhead_pressure()}")
+print(f"PSEUDOREDUCED TEMP(Tpr) = {pseudo_reduced_temp()}")
+print(f"PSEUDOREDUCED PRESSURE(Ppr) = {pseudo_reduced_wellhead_pressure()}")
 print('<-------------------------------------->')
 print('')
