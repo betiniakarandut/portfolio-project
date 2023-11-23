ecd63c2699a8d37da50f59f026712173ce679f96
betiniakarandut <betiniakarandut@gmail.com>
2023-11-23 16:06:08 +0100

    Function to locate cell with ppr

--- static_bhp/physical_properties/automate_integral.py
+++ static_bhp/physical_properties/automate_integral.py
@@ -1,12 +1,12 @@
-# import pandas as pd
+import pandas as pd
 # from conversions import evaluate_scrhs
 # from pseudocritical_properties import natural_gas_systems2
 from pseudoreduced_properties import pseudo_reduced_wellhead_pressure
 # from error_message import err_msg
 
 
-# df = pd.read_csv('../sukkarcornelintegral.csv')
-# df2 = pd.read_csv('../sukkarcornellintegral2.csv')
+df = pd.read_csv('../sukkarcornelintegral.csv')
+df2 = pd.read_csv('../sukkarcornellintegral2.csv')
 
 def truncate_to_one_dp(digit_to_truncate):
     """Truncates a real number to 1 decimal place
@@ -28,8 +28,26 @@ def truncate_to_one_dp(digit_to_truncate):
 def print_target_pseudoreduced_pressure():
     target_pseudoreduced_pressure = pseudo_reduced_wellhead_pressure()
     print(truncate_to_one_dp(target_pseudoreduced_pressure))
+
+# print_target_pseudoreduced_pressure()
     
-print_target_pseudoreduced_pressure()
+def locate_cell_with_ppr():
+    target_ppr = print_target_pseudoreduced_pressure()
+    print(target_ppr)
+    find_ppr = df["pseudoreduced_pressures"].searchsorted(target_ppr:target_ppr)
+    print('found it: ', find_ppr)
+    if find_ppr < 0 or find_ppr >= len(df):
+        return None
+    found_ppr = df['pseudoreduced_pressures'][find_ppr]
+    return found_ppr
+
+print(locate_cell_with_ppr())
+# isna_values = df['pseudoreduced_pressures'].isna()
+# if any(isna_values):
+#     print("The 'pseudoreduced_pressures' series contains None values.")
+# else:
+#     print('ppr found')
+# print_target_pseudoreduced_pressure()
 
 # target_pseudoreduced_pressure = pseudo_reduced_wellhead_pressure()
 # # print(target_pseudoreduced_pressure)
