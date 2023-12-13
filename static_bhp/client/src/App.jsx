bd4d5784ae8bbe311ba371dac65516a68c4e4e3c
betiniakarandut <betiniakarandut@gmail.com>
2023-12-13 14:16:40 +0100

    Changed to vite + react

---
+++ static_bhp/client/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <p>Welcome Betini</p>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
